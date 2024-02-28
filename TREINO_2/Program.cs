a61355cde531bf7ecc3840e11cda09fef62e26bc
Vitor Hugo <123844614+VH-DM@users.noreply.github.com>
2024-02-28 14:38:07 -0300

    Add files via upload

---
+++ TREINO_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> pessoas = new List<string>();
+
+        while (true)
+        {
+            Console.Write("Digite o nome da pessoa ou digite 'STOP': ");
+            string nome = Console.ReadLine().Trim();
+
+            if (nome.ToUpper() == "STOP")
+                break;
+
+            if (string.IsNullOrWhiteSpace(nome))
+            {
+                Console.WriteLine("Insira um nome válido.");
+                continue;
+            }
+            if (Regex.IsMatch(nome, @"[^a-zA-Z\s]"))
+            {
+                Console.WriteLine("Por favor, insira apenas caractéres válidos.");
+                continue;
+            }
+
+            pessoas.Add(nome);
+        }
+
+        Console.WriteLine("Lista de pessoas:");
+        foreach (string pessoa in pessoas)
+        {
+            Console.WriteLine("- {0}",pessoa);
+        }
+    }
+}
